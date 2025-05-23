677ace52272351cceff841b79e2223e07ae72e3d
Frederic <frederic1206@gmail.com>
2025-05-23 09:07:54 +0200

    feat: initialize cli (#1)

---
+++ cmd/root.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/thewhitewizard/web3data-cli/cmd/version"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:  "web3datacli",
+	Long: `A CLI tool to manage web3data`,
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	rootCmd.AddCommand(version.VersionCmd)
+}
