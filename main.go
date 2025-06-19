a915aff2455ea1641e5529e22eed1dde92b7e269
Frederic <frederic1206@gmail.com>
2025-06-19 11:05:32 +0200

    feat: fang integration (#8)

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"os"
+
+	"github.com/charmbracelet/fang"
 	"github.com/thewhitewizard/web3data-cli/cmd"
 )
 
 func main() {
-	cmd.Execute()
+	if err := fang.Execute(context.TODO(), cmd.RootCmd); err != nil {
+		os.Exit(1)
+	}
 }
